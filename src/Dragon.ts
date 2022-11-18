7e5844e6ebcb531ce7dae58ba44045199c0ac692
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-11-18 17:47:53 -0300

    feat(req12): added Dragon class

---
+++ src/Dragon.ts
@@ -0,0 +1,10 @@
+import Monster from './Monster';
+
+export default class Dragon extends Monster {
+  constructor(
+    lifePoints = 999,
+    strength = 63,
+  ) {
+    super(lifePoints, strength);
+  }
+}
